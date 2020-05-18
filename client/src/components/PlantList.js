1f65e7c83cc405876679c3aeea456fba7b5a5af4
craiglpaiva <craiglpaiva@gmail.com>
2020-05-17 23:52:41 -0400

    PlantsList Done

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
+
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
@@ -8,11 +8,22 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  state = {
+    plants: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3333/plants")
+      .then(res => res.json())
+      .then(plants => this.setState({ plants: plants.plantsData }))
+  }
+
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
@@ -29,11 +40,11 @@ export default class PlantList extends Component {
                 onClick={() => this.props.addToCart(plant)}
               >
                 Add to cart
-              </button>
+                </button>
             </div>
           </div>
         ))}
       </main>
     );
-  }
-}
+  };
+};
